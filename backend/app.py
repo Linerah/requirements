91b9d590a20321645c5ef02e6ebe482ef581d30b
Linerah <kevin.linera@upr.edu>
2022-03-15 15:36:54 -0400

    doing db work

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    day_scheduled = db.Column(db.String(200), nullable=False)
+    time_scheduled = db.Column(db.String(200), nullable=False)
+    user = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Schedule {self.id}, booked at {self.date_created}>'
 
 @app.route('/')
 def index():
