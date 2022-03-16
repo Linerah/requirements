66043510da833d89b9e35fbdde960e1fc07f451f
DevSebastian1 <sebastian.espinosa@upr.edu>
2022-03-16 19:22:01 -0400

    Added the User class

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import email
+from enum import unique
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -15,6 +17,19 @@ class Schedule(db.Model):
 
     def __repr__(self):
         return f'<Schedule {self.id}, booked at {self.date_created}>'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.id}'"
+
+class SlotTime(db.Model):
+    pass
+
 
 @app.route('/')
 def index():
